e7dc97f187b6ed17c607c98ed11506b312f1451c
mehremegashi <mg49217@universum-ks.org>
2021-04-09 13:47:11 +0200

    2nd commit
    Chapter5
    Chapter6
    Chapter7

---
+++ chapter6/detyra13/detyra13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace detyra13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a bin number");
+            int binnr = Console.ReadLine();
+            string decnr = Convert.ToInt32(binnr, 2);
+            Console.WriteLine(binnr + " ne decimal eshte " + decnr);
+        }
+    }
+}
+
